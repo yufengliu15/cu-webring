2e90433a0cfac2138cf69aee13896cbf4c0b77c2
yufengliu15 <yufeng.liu15@gmail.com>
2025-01-17 16:44:20 -0500

    base functionality achieved

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cu-webring
 A webring for Computer Science and Software Engineering students at Carleton University
+
+Heavily inspired by https://github.com/simcard0000/se-webring and https://github.com/leofalvo/mcgillcswebring.org. 
+
+## Features/Notes
+- Should randomize sites such that people at bottom can be seen at top
+- https://www.fusejs.io/getting-started/installation.html#direct-script-include
+- Do not use react or npm. This site should be built as lightweight and robust as possible
+- Keys: Full Name, Year Admitted into Carleton, Website URL
---
+++ js/user_sites.js
@@ -0,0 +1,84 @@
+const sites = [
+  {
+    "name": "Olivia Ochoa",
+    "year": 2025,
+    "website": "https://oliviaochoa.dev"
+  },
+  {
+    "name": "Jamie Bennett",
+    "year": 2022,
+    "website": "https://jamiebennett.io"
+  },
+  {
+    "name": "Pena Howard",
+    "year": 2022,
+    "website": "https://penahoward.tech"
+  },
+  {
+    "name": "Aguirre Becker",
+    "year": 2023,
+    "website": "https://aguirrebecker.dev"
+  },
+  {
+    "name": "Selena Franco",
+    "year": 2023,
+    "website": "https://selenafranco.io"
+  },
+  {
+    "name": "Bradley Cannon",
+    "year": 2024,
+    "website": "https://bradleycannon.dev"
+  },
+  {
+    "name": "Gilmore Petty",
+    "year": 2024,
+    "website": "https://gilmorepetty.tech"
+  },
+  {
+    "name": "Ada Jordan",
+    "year": 2022,
+    "website": "https://adajordan.io"
+  },
+  {
+    "name": "Sharon Kline",
+    "year": 2025,
+    "website": "https://sharonkline.dev"
+  },
+  {
+    "name": "Cohen Tucker",
+    "year": 2024,
+    "website": "https://cohentucker.tech"
+  },
+  {
+    "name": "Tracey Thompson",
+    "year": 2022,
+    "website": "https://traceythompson.io"
+  },
+  {
+    "name": "Moss Peters",
+    "year": 2023,
+    "website": "https://mosspeters.dev"
+  },
+  {
+    "name": "Mosley Frederick",
+    "year": 2024,
+    "website": "https://mosleyfrederick.tech"
+  },
+  {
+    "name": "Ramirez Warren",
+    "year": 2024,
+    "website": "https://ramirezwarren.io"
+  },
+  {
+    "name": "Wilkins Gibbs",
+    "year": 2023,
+    "website": "https://wilkinsgibbs.dev"
+  },
+  {
+    "name": "Danielle Contreras",
+    "year": 2022,
+    "website": "https://daniellecontreras.tech"
+  }
+];
+
+export default sites;
