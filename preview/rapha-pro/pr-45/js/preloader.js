cd5e709c392d39008b7180bde489b3a57eeee8b9
rapha-pro <rapha-pro@users.noreply.github.com>
2025-01-27 21:10:04 +0000

    deploy: 1ed58b44aedefa34270976b74b80bdfe8cd1be8a

---
+++ preview/rapha-pro/pr-45/js/preloader.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("content");
+        
+    if (content) {
+    content.classList.remove("hidden");
+    content.classList.add("visible");
+
+    document.body.style.overflow = "auto";
+    } else {
+    console.error("Content element not found!");
+    }
+});
+  
+  
