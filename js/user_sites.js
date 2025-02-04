5b42f9cd0b9a9eee0512bab09eac95b4e19c9415
yufengliu15 <yufeng.liu15@gmail.com>
2025-02-03 21:19:49 -0500

    added yufeng website

--- js/user_sites.js
+++ js/user_sites.js
@@ -1,12 +1,18 @@
 /* Format:
 {
-  "name": ,
+  "name": "",
   "year": ,
-  "website": ,
-  "major"
+  "website": "",
+  "major": ""
 },
 */
 const sites = [
+  {
+    "name": "Yufeng Liu",
+    "year": 2022,
+    "website": "https://yufengliu.tech",
+    "major": "Computer Science"
+  },
   {
     "name": "Jamie Bennett",
     "year": 2022,
