a4494183790d2b21f644096203de6efe1811503c
Robert Babaev <github@robertbabaev.tech>
2025-02-22 23:27:23 -0500

    add robertbabaev.tech + formatting from prettier

--- js/user_sites.js
+++ js/user_sites.js
@@ -9,71 +9,71 @@ Leave alias blank if you do not wish to use it.
 */
 const sites = [
   {
-    "name": "Yufeng Liu",
-    "year": 2022,
-    "website": "https://yufengliu.tech"
+    name: "Yufeng Liu",
+    year: 2022,
+    website: "https://yufengliu.tech",
   },
   {
-    "name": "Raphaël Onana",
-    "year": 2022,
-    "website": "https://nathonana.com"
+    name: "Raphaël Onana",
+    year: 2022,
+    website: "https://nathonana.com",
   },
   {
-    "name": "Ishar Ghura",
-    "year": 2022,
-    "website": "https://isharghura.com"
+    name: "Ishar Ghura",
+    year: 2022,
+    website: "https://isharghura.com",
   },
   {
-    "name": "Juan Marulanda",
-    "year": 2023,
-    "website": "https://juan-789.github.io/personal-website/",
-    "alias": "Juan Marulanda",
+    name: "Juan Marulanda",
+    year: 2023,
+    website: "https://juan-789.github.io/personal-website/",
+    alias: "Juan Marulanda",
   },
   {
-    "name": "Matteo Golin",
-    "year": 2021,
-    "website": "https://linguini1.github.io",
-    "alias": "linguini1.github.io",
+    name: "Matteo Golin",
+    year: 2021,
+    website: "https://linguini1.github.io",
+    alias: "linguini1.github.io",
   },
   {
-    "name": "Neel Patel",
-    "year": 2021,
-    "website": "https://notneelpatel.xyz",
+    name: "Neel Patel",
+    year: 2021,
+    website: "https://notneelpatel.xyz",
   },
   {
-    "name": "Matthew MacRae-Bovell",
-    "year": 2018,
-    "website": "https://matthewmacraebovell.com",
+    name: "Matthew MacRae-Bovell",
+    year: 2018,
+    website: "https://matthewmacraebovell.com",
   },
   {
-    "name": "Rebecca Kempe",
-    "year": 2022,
-    "website": "https://blog.rkempe.ca/",
+    name: "Rebecca Kempe",
+    year: 2022,
+    website: "https://blog.rkempe.ca/",
   },
   {
-    "name": "Riyan Ahmed",
-    "year": 2022,
-    "website": "https://riyanahmed.com",
-  },
-
-
-  {
-    "name": "Ansh Kakkar",
-    "year": 2023,
-    "website": "https://www.anshkakkar.dev/",
-
-  }, 
-  
-  {
-    "name": "Saim Hashmi",
-    "year": 2022,
-    "website": "https://saimhashmi.tech",
-
+    name: "Riyan Ahmed",
+    year: 2022,
+    website: "https://riyanahmed.com",
   },
   {
-    "name": "Jeremy Friesen",
-    "year": 2022,
-    "website": "https://jeremy-friesen.com",
+    name: "Ansh Kakkar",
+    year: 2023,
+    website: "https://www.anshkakkar.dev/",
+  },
+  {
+    name: "Saim Hashmi",
+    year: 2022,
+    website: "https://saimhashmi.tech",
+  },
+  {
+    name: "Jeremy Friesen",
+    year: 2022,
+    website: "https://jeremy-friesen.com",
+  },
+  {
+    name: "Robert Babaev",
+    year: 2019,
+    website: "https://robertbabaev.tech",
   },
 ];
 
